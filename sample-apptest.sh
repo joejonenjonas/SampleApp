01776413479a7e10659dca844098e2e5a8919fe9
Jonas Draps <73057343+joejonenjonas@users.noreply.github.com>
2021-12-15 18:09:56 +0100

    Update sample-apptest.sh

--- sample-apptest.sh
+++ sample-apptest.sh
@@ -1,6 +1,6 @@
 pwd
-#if curl http://172.17.0.1:8989 | grep gateway
-if curl http://ip6-localhost:8989 | grep gateway
+if curl http://172.17.0.1:8989 | grep gateway
+#if curl http://ip6-localhost:8989 | grep gateway
 then 
   exit 0
 else 
